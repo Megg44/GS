5f4de5038d3eec72953e842377a267832103dad2
Megg44 <76537255+Megg44@users.noreply.github.com>
2021-09-19 02:30:54 +0530

    Update Binary search.c

--- Binary search.c
+++ Binary search.c
@@ -4,21 +4,23 @@ int binarysearch(int n, int arr[n], int no){
   int begin = 0, end = n;
   int mid = (begin + end) / 2;
   int i;
-  while(arr[mid] != no){
-    if (arr[mid] > no)
+  while(begin < end){
+    if(arr[mid] == no)
+      return mid;
+    else if (arr[mid] > no)
       end = mid;
-    else
-      begin = mid;
+    else if (arr[mid] < no)
+      begin = mid + 1;
     mid = (begin + end) / 2;
   }
 
-  return mid;
+  return -1;
 }
 
 int main(){
   int arr[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-  int no = 80;
+  int no = 95;
 
   int i = binarysearch(10, arr, no);
-  printf("%dth place", i+1);
+  (i == -1) ? printf("Not found.") : printf("%dth place", i+1);
 }
